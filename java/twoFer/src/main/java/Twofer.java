34467e440d5dc91fe725ed6d6c27a9dd8d70a797
juri.castellani <1945Monterrey>
2018-10-18 20:48:42 +0200

    [Exercism] Two-fer first test has been turn to true.

--- java/twoFer/src/main/java/Twofer.java
+++ java/twoFer/src/main/java/Twofer.java
@@ -1,5 +1,8 @@
 class Twofer {
     String twofer(String name) {
+        if(name == null || name.equals("")) {
+            return "One for you, one for me.";
+        }
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 }
