da8b565bbed09bc1d85d3b4ed35ebcde6c5d94dc
juri.castellani <1945Monterrey>
2018-10-19 00:55:40 +0200

    [Exercism] Raindrops first test has been turn to true.

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,6 +1,9 @@
 class RaindropConverter {
 
     String convert(int number) {
+        if(number==1) {
+            return "1";
+        }
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
