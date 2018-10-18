582e081f2254c2f16c67719992fbf15b22ed055e
juri.castellani <1945Monterrey>
2018-10-19 01:14:08 +0200

    [Exercism] Code has been improved.

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -2,16 +2,20 @@ class RaindropConverter {
 
     String convert(int number) {
         String raindropString = "";
+        boolean isAtLeastAFactorFound = false;
         if(number % 3 == 0) {
             raindropString += "Pling";
+            isAtLeastAFactorFound = true;
         }
         if(number % 5 == 0) {
             raindropString += "Plang";
+            isAtLeastAFactorFound = true;
         }
         if(number % 7 == 0) {
             raindropString += "Plong";
+            isAtLeastAFactorFound = true;
         }
-        if(raindropString.length()==0) {
+        if(!isAtLeastAFactorFound) {
             raindropString = "" + number;
         }
         return raindropString;
