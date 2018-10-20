d6641cd021985af5c6084a66fb71afe308ec735a
juri.castellani <1945Monterrey>
2018-10-20 03:00:59 +0200

    [Exercism] Isogram first test has been turned to true.

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,6 +1,9 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
+        if(phrase.equals("")) {
+            return true;
+        }
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
