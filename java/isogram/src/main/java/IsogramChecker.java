acb3b9e70b54db81ec6ed0f754cc20f993ccf284
juri.castellani <1945Monterrey>
2018-10-20 13:57:21 +0200

    [Exercism] Isogram code has been improved removing useless code.

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -4,9 +4,6 @@ import java.util.List;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        if("".equals(phrase)) {
-            return true;
-        }
         List<Character> distinctCharacterList = new ArrayList<>();
         for (char c : phrase.toCharArray()) {
             if(distinctCharacterList.contains(c)) {
