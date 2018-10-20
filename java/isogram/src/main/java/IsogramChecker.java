8cff8bf47e6ecee590ae5e1d1e704e96648b89ea
juri.castellani <1945Monterrey>
2018-10-20 13:56:47 +0200

    [Exercism] Isogram second test has been turned to true.

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
         if("".equals(phrase)) {
             return true;
         }
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Character> distinctCharacterList = new ArrayList<>();
+        for (char c : phrase.toCharArray()) {
+            if(distinctCharacterList.contains(c)) {
+                return false;
+            }
+            distinctCharacterList.add(c);
+        }
+        return true;
     }
 
 }
