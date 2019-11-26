d7a3942a58d3f6df360f4761fde52c775abf8ce8
Juri Castellani <juri.castellani@hotmail.com>
2019-11-26 18:36:36 +0100

    [exercism] hamming: all tests have been made true.

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -5,12 +5,24 @@ class Hamming {
     Hamming(String leftStrand, String rightStrand) {
         this.leftStrand = leftStrand;
         this.rightStrand = rightStrand;
+        if(leftStrand.length() == 0 && rightStrand.length() > 0) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if(leftStrand.length() > 0 && rightStrand.length() == 0) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        if(leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
-    int getHammingDistance() throws Exception {
-        if(leftStrand.length() != rightStrand.length()) {
-            throw new Exception("Strands don't have the same length.");
+    int getHammingDistance() throws IllegalArgumentException {
+        int distance = 0;
+        for(int i=0; i<leftStrand.length(); i++) {
+            if(leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                distance++;
+            }
         }
-        return 0;
+        return distance;
     }
 }
