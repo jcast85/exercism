a3a178f50b92bf9987dffae931e8fa07bc9342b2
Juri Castellani <juri.castellani@hotmail.com>
2019-11-27 02:12:02 +0100

    [exercism] reverse-string: exercise has been done.

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,11 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String reverseString = "";
+        for(int i=inputString.length()-1; i>=0; i--) {
+            reverseString += inputString.charAt(i);
+        }
+        return reverseString;
     }
   
 }
