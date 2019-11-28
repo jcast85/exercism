1d6969cee34d9731f379713757a73e83b711a6b4
Juri Castellani <juri.castellani@hotmail.com>
2019-11-28 17:57:49 +0100

    [exercism] pig-latin: the most of the tests have been turned to truth.

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,10 +1,61 @@
-/*
+import java.util.Arrays;
+import java.util.List;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class PigLatinTranslator {
+    private final static List<String> vowelList = Arrays.asList("a", "e", "i", "o", "u");
+    private final static List<String> consonantList = Arrays.asList("b", "c", "d", "f", "g", "h",
+            "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z");
 
-Please remove this comment when submitting your solution.
+    String translate(String word) {
+        if(isAVowelSoundWord(word)) {
+            return addAyAtTheEnd(word);
+        } else {
+            return addAyAtTheEnd(getWordWithConsonantSoundPartMovedAtTheEnd(word));
+        }
+    }
 
-*/
+    private static boolean isAVowelSoundWord(String word) {
+        return vowelList.contains(String.valueOf(word.charAt(0)))
+                || word.startsWith("xr") || word.startsWith("yt");
+    }
+
+    private static String addAyAtTheEnd(String word) {
+        return word + "ay";
+    }
+
+    private static String getWordWithConsonantSoundPartMovedAtTheEnd(String word) {
+        StringBuilder wordWithConsonantSoundPartMovedAtTheEnd = new StringBuilder();
+        String consonantSoundPart = getConsonantSoundPart(word);
+        String wordWithoutConsonantSoundPart = word.replace(consonantSoundPart, "");
+        if(isQuCase(wordWithoutConsonantSoundPart, consonantSoundPart)) {
+            return wordWithConsonantSoundPartMovedAtTheEnd.append(wordWithoutConsonantSoundPart.replace("u", ""))
+                    .append(consonantSoundPart)
+                    .append("u")
+                    .toString();
+        }
+        return wordWithConsonantSoundPartMovedAtTheEnd.append(wordWithoutConsonantSoundPart)
+                .append(consonantSoundPart)
+                .toString();
+    }
+
+    private static boolean isQuCase(String wordWithoutConsonantSoundPart, String consonantSoundPart) {
+        if(wordWithoutConsonantSoundPart.length() == 0 || consonantSoundPart.length() == 0) {
+            return false;
+        }
+        return consonantSoundPart.charAt(consonantSoundPart.length() -1) == 'q' && wordWithoutConsonantSoundPart.charAt(0) == 'u';
+    }
+
+    private static String getConsonantSoundPart(String word) {
+        // pre-condition: word starts with a consonant
+        // two chars cases: ch, gh, ph, th, sh, wh
+        StringBuilder consonantSoundPart = new StringBuilder();
+        for(char charI : word.toCharArray()) {
+            if (consonantList.contains(String.valueOf(charI))) {
+                consonantSoundPart.append(charI);
+            } else {
+                break;
+            }
+        }
+        return consonantSoundPart.toString();
+    }
+}
