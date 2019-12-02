3d702cabf9bfc6230336e3d9dbea6cbe57acf62a
Juri Castellani <juri.castellani@hotmail.com>
2019-12-02 01:23:43 +0100

    [exercism] anagram: all tests have been turned to truth.

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,10 +1,39 @@
-/*
+import java.util.List;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class Anagram {
+    private final String word;
 
-Please remove this comment when submitting your solution.
+    public Anagram(String word) {
+        this.word = word;
+    }
 
-*/
+    public List<String> match(List<String> possibleAnagramList) {
+        return possibleAnagramList.stream()
+                .filter(this::isAnagram)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isAnagram(String possibleAnagram) {
+        if(possibleAnagram.length() != word.length()) {
+            return false;
+        }
+        String lowerCaseWord = word.toLowerCase();
+        String lowerCasePossibleAnagram = possibleAnagram.toLowerCase();
+        if(lowerCasePossibleAnagram.equals(lowerCaseWord)) {
+            return false;
+        }
+        for (char currentChar : lowerCasePossibleAnagram.toCharArray()) {
+            if(isContained(currentChar, lowerCaseWord)) {
+                lowerCaseWord = lowerCaseWord.replaceFirst(String.valueOf(currentChar), "");
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isContained(char currentChar, String lowerCaseWord) {
+        return lowerCaseWord.contains(String.valueOf(currentChar));
+    }
+}
