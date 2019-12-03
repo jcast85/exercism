7e087cfe74ac05ab8d5aafdb832d481e00d1fcb1
Juri Castellani <juri.castellani@hotmail.com>
2019-12-03 07:10:41 +0100

    [exercism] proverb: tests have been turned into truth.

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,27 @@
 class Proverb {
+    private final String[] words;
 
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder stringBuilder = new StringBuilder();
+        if (words.length == 0) {
+            return stringBuilder.toString();
+        }
+        for(int i=0; i<words.length-1; i++) {
+            stringBuilder
+                    .append("For want of a ")
+                    .append(words[i])
+                    .append(" the ")
+                    .append(words[i+1])
+                    .append(" was lost.\n");
+        }
+        return stringBuilder
+                .append("And all for the want of a ")
+                .append(words[0])
+                .append(".")
+                .toString();
     }
-
 }
