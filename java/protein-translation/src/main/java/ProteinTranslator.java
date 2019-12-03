a1ca4a81be308602bdfee1f457e05e871af02fea
Juri Castellani <juri.castellani@hotmail.com>
2019-12-03 01:15:32 +0100

    [exercism] protein-translation: some tests have been turned to truth. Some warnings have been fixed.

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class ProteinTranslator {
-
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<String> proteinList = new ArrayList<>();
+        String possibleProteinSequence = rnaSequence.split("UAA")[0];
+        while (possibleProteinSequence.length()>2) {
+            String currentPossibleProtein = possibleProteinSequence.substring(0, 3);
+            possibleProteinSequence = possibleProteinSequence.replaceFirst(currentPossibleProtein, "");
+            if(isAProtein(currentPossibleProtein)) {
+                String progeinName = getProteinName(currentPossibleProtein);
+                if(!proteinList.contains(progeinName)) {
+                    proteinList.add(progeinName);
+                }
+            }
+        }
+        return proteinList;
     }
+
+    private static String getProteinName(String currentPossibleProtein) {
+        return proteinNameMap.get(currentPossibleProtein);
+    }
+
+    private static boolean isAProtein(String currentPossibleProtein) {
+        return proteinNameMap.keySet().contains(currentPossibleProtein);
+    }
+
+    private final static Map<String, String> proteinNameMap = new HashMap<>() {{
+        put("AUG", "Methionine");
+        put("UUU", "Phenylalanine");
+        put("UUC", "Phenylalanine");
+        put("UUA", "Leucine");
+        put("UUG", "Leucine");
+        put("UCU", "Serine");
+        put("UCC", "Serine");
+        put("UCA", "Serine");
+        put("UCG", "Serine");
+        put("UAU", "Tyrosine");
+        put("UAC", "Tyrosine");
+        put("UGU", "Cysteine");
+        put("UGC", "Cysteine");
+        put("UGG", "Tryptophan");
+    }};
 }
