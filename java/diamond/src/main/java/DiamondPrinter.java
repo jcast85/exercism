8be38a35f6eb70992ebb49e350f621c4228e88d0
Juri Castellani <juri.castellani@hotmail.com>
2019-12-09 18:13:51 +0100

    [exercism] diamond: code readability has been improved.

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -8,23 +8,29 @@ class DiamondPrinter {
         List<String> diamond = new ArrayList<>();
         int blankSpaceNumber = a - aIntValue;
         int charNumberPerRow = 2*blankSpaceNumber+1;
+
         int currentRowPrefixBlankNumber = blankSpaceNumber;
         boolean isDiamondBuildIncreasing = true;
         for(int i=0; i<charNumberPerRow; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(" ".repeat(Math.max(0, currentRowPrefixBlankNumber)));
             int internBlankSpaceNumber = charNumberPerRow - 2*currentRowPrefixBlankNumber - 2;
             char charToAdd = (char) (aIntValue + blankSpaceNumber - currentRowPrefixBlankNumber);
-            stringBuilder.append(charToAdd);
+
+            StringBuilder stringBuilder = new StringBuilder()
+                    .append(addBlankSpaces(currentRowPrefixBlankNumber))
+                    .append(charToAdd);
             if(internBlankSpaceNumber>0) {
-                stringBuilder.append(" ".repeat(Math.max(0, internBlankSpaceNumber)));
-                stringBuilder.append(charToAdd);
+                stringBuilder
+                        .append(addBlankSpaces(internBlankSpaceNumber))
+                        .append(charToAdd);
             }
-            stringBuilder.append(" ".repeat(Math.max(0, currentRowPrefixBlankNumber)));
+            stringBuilder
+                    .append(addBlankSpaces(currentRowPrefixBlankNumber));
             diamond.add(stringBuilder.toString());
+
             if(currentRowPrefixBlankNumber==0) {
                 isDiamondBuildIncreasing = false;
             }
+
             if(isDiamondBuildIncreasing) {
                 currentRowPrefixBlankNumber--;
             } else {
@@ -32,6 +38,10 @@ class DiamondPrinter {
             }
         }
         return diamond;
+    }
+
+    private String addBlankSpaces(int currentRowPrefixBlankNumber) {
+        return " ".repeat(Math.max(0, currentRowPrefixBlankNumber));
     }
 
 }
